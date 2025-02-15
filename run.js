5d13442d888cd9a7d945b1cafdab789f6ccae244
envyst <nurardli.saputra@gmail.com>
2025-02-15 12:14:33 +0700

    initial fork and some update

---
+++ run.js
@@ -0,0 +1,15 @@
+import { spawn } from "child_process";
+
+function startProcess() {
+  console.log("Starting WalletDashboard...");
+
+  const process = spawn("node", ["main.js"], { stdio: "inherit" });
+
+  setTimeout(() => {
+    console.log("Restarting WalletDashboard after 1 hour...");
+    process.kill(); // Kill the current process
+    startProcess(); // Restart the process
+  }, 3600000); // 1 hour
+}
+
+startProcess();
